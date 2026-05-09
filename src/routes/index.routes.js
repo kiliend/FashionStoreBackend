f3a6202e6ae89ce4b2201b19b8040a8b0b0048ed
kiliend <luis.condori.h98@gmail.com>
2026-05-08 21:18:09 -0500

    Se realizan pruebas de conexion con la base de datos

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../config/db");
 
 router.get("/", (req, res) => {
   res.json({
     ok: true,
     message: "API FashionStore funcionando correctamente"
   });
+});
+
+router.get("/test-db", async (req, res, next) => {
+  try {
+    const [rows] = await pool.query("SELECT NOW() AS fecha_servidor");
+
+    res.json({
+      ok: true,
+      message: "Conexión a MySQL correcta",
+      data: rows[0]
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
