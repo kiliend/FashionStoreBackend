90947559a69579919425e25c8a3db9484302df13
kiliend <luis.condori.h98@gmail.com>
2026-05-08 21:40:26 -0500

    implementacion de la ruta de usuarios

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,10 +1,43 @@
 const express = require("express");
 const router = express.Router();
 
-const authController = require("./auth.controller");
-const { verifyToken } = require("./auth.middleware");
+const usuariosController = require("./usuarios.controller");
+const { verifyToken } = require("../auth/auth.middleware");
+const { authorizeRoles } = require("../../middlewares/role.middleware");
 
-router.post("/login", authController.login);
-router.get("/profile", verifyToken, authController.profile);
+router.get(
+  "/",
+  verifyToken,
+  authorizeRoles("admin"),
+  usuariosController.listarUsuarios
+);
+
+router.get(
+  "/:id",
+  verifyToken,
+  authorizeRoles("admin"),
+  usuariosController.obtenerUsuario
+);
+
+router.post(
+  "/",
+  verifyToken,
+  authorizeRoles("admin"),
+  usuariosController.crearUsuario
+);
+
+router.put(
+  "/:id",
+  verifyToken,
+  authorizeRoles("admin"),
+  usuariosController.actualizarUsuario
+);
+
+router.delete(
+  "/:id",
+  verifyToken,
+  authorizeRoles("admin"),
+  usuariosController.eliminarUsuario
+);
 
 module.exports = router;
